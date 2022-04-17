b87ed981f93febc2c03728fd5923db3f4e5266b9
syworld <syworldy@gmail.com>
2022-04-18 01:12:49 +0900

    Initial commit

---
+++ src/main/java/com/world/playstay/PlaystayApplication.java
@@ -0,0 +1,13 @@
+package com.world.playstay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaystayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaystayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/world/playstay/PlaystayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.world.playstay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaystayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
