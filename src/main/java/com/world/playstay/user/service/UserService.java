8775f88630cdbbed1f200f6815e514fd39df4362
syworld <syworldy@gmail.com>
2022-05-08 19:49:17 +0900

    #3 feat: UserNotFoundException 커스텀 에러 설정

--- src/main/java/com/world/playstay/user/service/UserService.java
+++ src/main/java/com/world/playstay/user/service/UserService.java
@@ -1,7 +1,9 @@
 package com.world.playstay.user.service;
 
 import com.world.playstay.user.dto.UserDto;
+import com.world.playstay.user.exception.UserNotFoundException;
 import com.world.playstay.user.mapper.UserMapper;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +17,11 @@ public class UserService {
 
   public void join(UserDto userDto) {
     userMapper.insert(userDto);
+  }
+
+  public UserDto findUser(Long id) {
+    Optional<UserDto> user = Optional.ofNullable(userMapper.findById(id));
+    return user.orElseThrow(() -> new UserNotFoundException("User does not exist"));
   }
 
 }
