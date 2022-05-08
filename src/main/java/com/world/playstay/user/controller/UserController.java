f3fc8c319af7af34fd9192d2c1bab7a001864a9e
syworld <syworldy@gmail.com>
2022-05-08 19:49:50 +0900

    feat: ResponseBody 적용

--- src/main/java/com/world/playstay/user/controller/UserController.java
+++ src/main/java/com/world/playstay/user/controller/UserController.java
@@ -2,13 +2,18 @@ package com.world.playstay.user.controller;
 
 import com.world.playstay.user.dto.UserDto;
 import com.world.playstay.user.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@ResponseBody
 @RequestMapping("/users")
 public class UserController {
 
@@ -21,6 +26,12 @@ public class UserController {
   @PostMapping()
   public void create(UserDto userDto) {
     userService.join(userDto);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<UserDto> findUserById(@PathVariable("id") Long id) {
+    UserDto userDto = userService.findUser(id);
+    return ResponseEntity.ok().body(userDto);
   }
 
 }
