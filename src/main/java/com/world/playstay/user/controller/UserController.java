ab84c9b7c091d6e75a4ff51725cf73fc68253d27
syworld <syworldy@gmail.com>
2022-05-11 23:01:55 +0900

    #3 refactor: RequiredArgsConstructor으로 빈 인젝션 처리

--- src/main/java/com/world/playstay/user/controller/UserController.java
+++ src/main/java/com/world/playstay/user/controller/UserController.java
@@ -2,25 +2,21 @@ package com.world.playstay.user.controller;
 
 import com.world.playstay.user.dto.UserDto;
 import com.world.playstay.user.service.UserService;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
   private final UserService userService;
-
-  public UserController(UserService userService) {
-    this.userService = userService;
-  }
 
   @PostMapping()
   public void create(UserDto userDto) {
--- src/main/java/com/world/playstay/user/service/UserService.java
+++ src/main/java/com/world/playstay/user/service/UserService.java
@@ -4,16 +4,14 @@ import com.world.playstay.user.dto.UserDto;
 import com.world.playstay.user.exception.UserNotFoundException;
 import com.world.playstay.user.mapper.UserMapper;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
   private final UserMapper userMapper;
-
-  public UserService(UserMapper userMapper) {
-    this.userMapper = userMapper;
-  }
 
   public void join(UserDto userDto) {
     userMapper.insert(userDto);
