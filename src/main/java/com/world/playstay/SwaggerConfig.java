417a6f89f7d698c58de9ef89368cca39e158fab8
syworld <syworldy@gmail.com>
2022-05-24 23:13:45 +0900

    #3 feat: swagger 설정

---
+++ src/main/java/com/world/playstay/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.world.playstay;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public GroupedOpenApi publicApi() {
+    return GroupedOpenApi.builder()
+        .group("v1")
+        .packagesToScan("com.world")
+        .build();
+  }
+
+  @Bean
+  public OpenAPI springOpenAPI() {
+    return new OpenAPI()
+        .info(new Info().title("Play and Stay").description("Play and Stay API document").version("v0.0.1"));
+  }
+}
--- src/main/java/com/world/playstay/user/controller/UserController.java
+++ src/main/java/com/world/playstay/user/controller/UserController.java
@@ -1,7 +1,16 @@
 package com.world.playstay.user.controller;
 
+import com.world.playstay.global.error.GlobalExceptionResponse;
 import com.world.playstay.user.dto.UserDto;
 import com.world.playstay.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Tag(name = "users")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -21,19 +30,25 @@ public class UserController {
   private final UserService userService;
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+  @Operation(summary = "유저 생성")
+  @ApiResponse(responseCode = "200")
   @PostMapping()
   public void create(UserDto userDto) {
     userService.join(userDto);
   }
 
+  @Operation(summary = "유저 조회", description = "유저 id로 유저 조회하기")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = UserDto.class))),
+      @ApiResponse(responseCode = "404", description = "USER NOT FOUND", content = @Content(schema = @Schema(implementation = GlobalExceptionResponse.class))),
+      @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = @Content(schema = @Schema(implementation = GlobalExceptionResponse.class))),
+  })
+  @Parameters({
+      @Parameter(name = "id", description = "아이디", example = "1"),
+  })
   @GetMapping("/{id}")
   public ResponseEntity<UserDto> findUserById(@PathVariable("id") Long id) {
     UserDto userDto = userService.findUser(id);
-    logger.trace("trace log");
-    logger.debug("debug log");
-    logger.info("info log");
-    logger.warn("warn log");
-    logger.error("error log");
     return ResponseEntity.ok().body(userDto);
   }
 
