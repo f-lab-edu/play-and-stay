db3e82dbc70644ad458e249c7c3a1fb556ca9624
syworld <syworldy@gmail.com>
2022-06-02 06:39:28 +0900

    #3 chore: 정보 추가

--- src/main/java/com/world/playstay/user/controller/UserController.java
+++ src/main/java/com/world/playstay/user/controller/UserController.java
@@ -16,12 +16,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "users")
@@ -33,28 +36,29 @@ public class UserController {
   private final UserService userService;
   private final UserMapstructMapper userMapstructMapper;
 
-  private UserDto toDto(User user){
+  private UserDto toDto(User user) {
     return userMapstructMapper.toDto(user);
   }
 
-  private User toEntity(UserDto userDto){
+  private User toEntity(UserDto userDto) {
     return userMapstructMapper.toEntity(userDto);
   }
 
   @Operation(summary = "유저 생성")
   @ApiResponses({
-      @ApiResponse(responseCode = "201", description = "USER CREATED", content = @Content(schema = @Schema(implementation = UserDto.class))),
+      @ApiResponse(responseCode = "201", description = "USER CREATED"),
       @ApiResponse(responseCode = "400", description = "DUPLICATED USER", content = @Content(schema = @Schema(implementation = GlobalExceptionResponse.class))),
   })
+  @ResponseStatus(HttpStatus.CREATED)
   @PostMapping()
-  public void createUser(UserDto userDto) {
+  public void createUser(@RequestBody UserDto userDto) {
     User user = toEntity(userDto);
     userService.join(user);
   }
 
   @Operation(summary = "유저 삭제")
   @ApiResponses({
-      @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = UserDto.class))),
+      @ApiResponse(responseCode = "200", description = "OK"),
       @ApiResponse(responseCode = "404", description = "USER NOT FOUND", content = @Content(schema = @Schema(implementation = GlobalExceptionResponse.class))),
   })
   @Parameters({
@@ -80,7 +84,7 @@ public class UserController {
   }
 
   @Operation(summary = "모든 유저 조회")
-  @ApiResponse(responseCode = "200")
+  @ApiResponse(responseCode = "200", description = "OK")
   @GetMapping()
   public List<UserDto> getUsers() {
     return userService.getUsers().stream()
