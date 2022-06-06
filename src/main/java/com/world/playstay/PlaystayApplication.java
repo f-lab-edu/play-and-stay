d179cee2d21674105645d898cd7db9c20783a7a6
syworld <syworldy@gmail.com>
2022-06-06 15:51:24 +0900

    #3 refactor: sl4j 어노테이션 사용해서 로깅

--- src/main/java/com/world/playstay/PlaystayApplication.java
+++ src/main/java/com/world/playstay/PlaystayApplication.java
@@ -1,34 +1,34 @@
 package com.world.playstay;
 
 import java.util.Arrays;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 
+@Slf4j
 @SpringBootApplication
 public class PlaystayApplication {
-  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   public static void main(String[] args) {
     SpringApplication.run(PlaystayApplication.class, args);
   }
 
-
+  @Profile("local")
   @Bean
   public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
     return args -> {
 
-      logger.debug("Let's inspect the beans provided by Spring Boot:");
+      log.debug("Let's inspect the beans provided by Spring Boot:");
 
       String[] beanNames = ctx.getBeanDefinitionNames();
       Arrays.sort(beanNames);
       for (String beanName : beanNames) {
-        logger.debug("BEAN: {}", beanName);
+        log.debug("BEAN: {}", beanName);
       }
 
     };
--- src/main/java/com/world/playstay/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/world/playstay/global/error/GlobalExceptionHandler.java
@@ -1,8 +1,7 @@
 package com.world.playstay.global.error;
 
 import javax.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,33 +12,40 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * 모든 RuntimeException을 공통 response format인 GlobalExceptionResponse로 처리한다.
  * 프로젝트에서 정의하지 않은 Exception이 발생하면 INTERNAL SERVER ERROR로 처리한다.
  */
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-  public void createLog(LogLevel logLevel, String errorCode, String message, String requestPath){
-    if (logLevel == LogLevel.NONE){
+  public void createLog(LogLevel logLevel, String errorCode, String message, String requestPath) {
+    if (logLevel == LogLevel.NONE) {
       return;
     }
-    if (logLevel == LogLevel.ERROR){
-      logger.error("[EXCEPTION] path: {} | errorCode: {} | message: {}", requestPath, errorCode, message);
-    }
-    else{
-      logger.warn("[EXCEPTION] path: {} | errorCode: {} | message: {}", requestPath, errorCode, message);
+    if (logLevel == LogLevel.ERROR) {
+      log.error("[EXCEPTION] path: {} | errorCode: {} | message: {}", requestPath, errorCode,
+          message);
+    } else {
+      log.warn("[EXCEPTION] path: {} | errorCode: {} | message: {}", requestPath, errorCode,
+          message);
     }
   }
 
   @ExceptionHandler(GlobalHttpException.class)
-  public ResponseEntity<GlobalExceptionResponse> handleGlobalHttpException(GlobalHttpException e,  HttpServletRequest request){
-    GlobalExceptionResponse response = new GlobalExceptionResponse(e.getStatus(), e.getClass().getSimpleName(), e.getMessage(), request.getRequestURI());
-    createLog(e.getLogLevel(),  e.getClass().getSimpleName(), e.getMessage(), request.getRequestURI());
+  public ResponseEntity<GlobalExceptionResponse> handleGlobalHttpException(GlobalHttpException e,
+      HttpServletRequest request) {
+    GlobalExceptionResponse response = new GlobalExceptionResponse(e.getStatus(),
+        e.getClass().getSimpleName(), e.getMessage(), request.getRequestURI());
+    createLog(e.getLogLevel(), e.getClass().getSimpleName(), e.getMessage(),
+        request.getRequestURI());
     return ResponseEntity.status(e.getStatus()).body(response);
   }
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<GlobalExceptionResponse> handleException(Exception e, HttpServletRequest request){
-    GlobalExceptionResponse response = new GlobalExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getClass().getSimpleName(), e.getMessage(), request.getRequestURI());
-    createLog(LogLevel.ERROR,  e.getClass().getSimpleName(), e.getMessage(), request.getRequestURI());
+  public ResponseEntity<GlobalExceptionResponse> handleException(Exception e,
+      HttpServletRequest request) {
+    GlobalExceptionResponse response = new GlobalExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR,
+        e.getClass().getSimpleName(), e.getMessage(), request.getRequestURI());
+    createLog(LogLevel.ERROR, e.getClass().getSimpleName(), e.getMessage(),
+        request.getRequestURI());
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
   }
 }
