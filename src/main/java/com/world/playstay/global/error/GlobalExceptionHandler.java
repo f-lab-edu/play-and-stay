1331d874f1080b21fe50305ce4791744bdad21d5
syworld <syworldy@gmail.com>
2022-06-06 16:25:43 +0900

    #3 refactor: error message format 분리, 에러 스택 트레이스 처리

--- src/main/java/com/world/playstay/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/world/playstay/global/error/GlobalExceptionHandler.java
@@ -7,25 +7,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-/**
- * 전역 예외 처리 핸들러
- * 모든 RuntimeException을 공통 response format인 GlobalExceptionResponse로 처리한다.
- * 프로젝트에서 정의하지 않은 Exception이 발생하면 INTERNAL SERVER ERROR로 처리한다.
+/*
+ 전역 예외 처리 핸들러
+ 모든 RuntimeException을 공통 response format인 GlobalExceptionResponse로 처리한다.
+ 프로젝트에서 정의하지 않은 Exception이 발생하면 INTERNAL SERVER ERROR로 처리한다.
  */
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  public void createLog(LogLevel logLevel, String errorCode, String message, String requestPath) {
+  private static final String ERROR_MESSAGE_FORMAT = "[EXCEPTION] path: {} | errorCode: {} | message: {}";
+
+  public void createLog(LogLevel logLevel, Exception e, String requestPath) {
     if (logLevel == LogLevel.NONE) {
       return;
     }
     if (logLevel == LogLevel.ERROR) {
-      log.error("[EXCEPTION] path: {} | errorCode: {} | message: {}", requestPath, errorCode,
-          message);
+      log.error(ERROR_MESSAGE_FORMAT, requestPath,
+          e.getClass().getSimpleName(),
+          e.getMessage(), e);
     } else {
-      log.warn("[EXCEPTION] path: {} | errorCode: {} | message: {}", requestPath, errorCode,
-          message);
+      log.warn(ERROR_MESSAGE_FORMAT, requestPath,
+          e.getClass().getSimpleName(),
+          e.getMessage(), e);
     }
   }
 
@@ -34,8 +38,7 @@ public class GlobalExceptionHandler {
       HttpServletRequest request) {
     GlobalExceptionResponse response = new GlobalExceptionResponse(e.getStatus(),
         e.getClass().getSimpleName(), e.getMessage(), request.getRequestURI());
-    createLog(e.getLogLevel(), e.getClass().getSimpleName(), e.getMessage(),
-        request.getRequestURI());
+    createLog(e.getLogLevel(), e, request.getRequestURI());
     return ResponseEntity.status(e.getStatus()).body(response);
   }
 
@@ -44,8 +47,7 @@ public class GlobalExceptionHandler {
       HttpServletRequest request) {
     GlobalExceptionResponse response = new GlobalExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR,
         e.getClass().getSimpleName(), e.getMessage(), request.getRequestURI());
-    createLog(LogLevel.ERROR, e.getClass().getSimpleName(), e.getMessage(),
-        request.getRequestURI());
+    createLog(LogLevel.ERROR, e, request.getRequestURI());
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
   }
 }
