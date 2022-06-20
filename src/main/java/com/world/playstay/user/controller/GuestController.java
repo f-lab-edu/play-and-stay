efaff5ded660fae641b71587ca21febfaa7c5e04
syworld <syworldy@gmail.com>
2022-06-20 19:49:38 +0900

    #5 feat: user domain controller 구현

---
+++ src/main/java/com/world/playstay/user/controller/GuestController.java
@@ -0,0 +1,54 @@
+package com.world.playstay.user.controller;
+
+import com.world.playstay.global.error.GlobalExceptionResponse;
+import com.world.playstay.user.dto.request.CreateGuestRequest;
+import com.world.playstay.user.dto.response.GuestResponse;
+import com.world.playstay.user.entity.Guest;
+import com.world.playstay.user.mapper.GuestMapstructMapper;
+import com.world.playstay.user.service.GuestService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "guests")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/guests")
+public class GuestController {
+
+  private final GuestService guestService;
+  private final GuestMapstructMapper guestMapstructMapper;
+
+
+  private Guest toEntity(CreateGuestRequest guestRequest) {
+    return guestMapstructMapper.toGuest(guestRequest);
+  }
+
+  private GuestResponse toResponse(Guest guest) {
+    return guestMapstructMapper.toResponse(guest);
+  }
+
+  @Operation(summary = "회원가입")
+  @ApiResponses({
+      @ApiResponse(responseCode = "201", description = "GUEST CREATED"),
+      @ApiResponse(responseCode = "400", description = "DUPLICATED GUEST", content = @Content(schema = @Schema(implementation = GlobalExceptionResponse.class))),
+  })
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping()
+  public void create(@RequestBody @Validated CreateGuestRequest createGuestRequest) {
+    Guest guest = toEntity(createGuestRequest);
+    guestService.join(guest, createGuestRequest.getPassword());
+  }
+
+}
