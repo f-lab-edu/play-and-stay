bcffe87c106beebd5f25ae19d53c84b62877a12e
syworld <syworldy@gmail.com>
2022-06-20 19:48:47 +0900

    #5 refactor: UserMapper 제거

--- src/main/java/com/world/playstay/user/dao/GuestMapper.java
+++ src/main/java/com/world/playstay/user/dao/GuestMapper.java
@@ -2,10 +2,22 @@ package com.world.playstay.user.dao;
 
 
 import com.world.playstay.user.entity.Guest;
+import java.util.List;
+import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface GuestMapper extends UserMapper<Guest> {
+public interface GuestMapper {
+
+  void insert(Guest guest);
+
+  void delete(Long id);
+
+  Optional<Guest> findById(Long id);
+
+  Optional<Guest> findByEmail(String email);
+
+  List<Guest> findAll();
 
 }
 
--- src/main/java/com/world/playstay/user/dao/HostMapper.java
+++ src/main/java/com/world/playstay/user/dao/HostMapper.java
@@ -1,9 +1,20 @@
 package com.world.playstay.user.dao;
 
 import com.world.playstay.user.entity.Host;
+import java.util.List;
+import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface HostMapper extends UserMapper<Host> {
+public interface HostMapper {
 
+  void insert(Host user);
+
+  void delete(Long id);
+
+  Optional<Host> findById(Long id);
+
+  Optional<Host> findByEmail(String email);
+
+  List<Host> findAll();
 }
