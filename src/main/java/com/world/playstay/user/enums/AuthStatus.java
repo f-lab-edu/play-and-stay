b0296f130d81a531f786a1a1809af6a76d681808
syworld <syworldy@gmail.com>
2022-07-10 20:36:35 +0900

    #10 feat: TypeHandler mappedTypes 지정

--- src/main/java/com/world/playstay/user/enums/AuthStatus.java
+++ src/main/java/com/world/playstay/user/enums/AuthStatus.java
@@ -1,6 +1,30 @@
 package com.world.playstay.user.enums;
 
-public enum AuthStatus {
-  UNAUTHENTICATED,
-  AUTHENTICATED,
+import com.world.playstay.global.enums.GlobalEnum;
+import com.world.playstay.global.enums.GlobalEnumTypeHandler;
+import org.apache.ibatis.type.MappedTypes;
+
+public enum AuthStatus implements GlobalEnum {
+  UNAUTHENTICATED("UNAUTHENTICATED"),
+  AUTHENTICATED("AUTHENTICATED");
+
+  private final String code;
+
+  AuthStatus(String code) {
+    this.code = code;
+  }
+
+  @Override
+  public String getCode() {
+    return code;
+  }
+
+  @MappedTypes(AuthStatus.class)
+  public static class TypeHandler extends GlobalEnumTypeHandler<AuthStatus> {
+
+    public TypeHandler() {
+      super(AuthStatus.class);
+    }
+  }
+
 }
--- src/main/java/com/world/playstay/user/enums/MemberShipStatus.java
+++ src/main/java/com/world/playstay/user/enums/MemberShipStatus.java
@@ -1,7 +1,30 @@
 package com.world.playstay.user.enums;
 
-public enum MemberShipStatus {
-  BASIC,
-  STANDARD,
-  PREMIUM
+import com.world.playstay.global.enums.GlobalEnum;
+import com.world.playstay.global.enums.GlobalEnumTypeHandler;
+import org.apache.ibatis.type.MappedTypes;
+
+public enum MembershipStatus implements GlobalEnum {
+  BASIC("BASIC"),
+  STANDARD("STANDARD"),
+  PREMIUM("PREMIUM");
+
+  private final String code;
+
+  MembershipStatus(String code) {
+    this.code = code;
+  }
+
+  @Override
+  public String getCode() {
+    return code;
+  }
+
+  @MappedTypes(MembershipStatus.class)
+  public static class TypeHandler extends GlobalEnumTypeHandler<MembershipStatus> {
+
+    public TypeHandler() {
+      super(MembershipStatus.class);
+    }
+  }
 }
