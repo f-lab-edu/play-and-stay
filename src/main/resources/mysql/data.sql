7eb44b20f1af691e1deb2385042496274787f07b
syworld <syworldy@gmail.com>
2022-07-10 20:39:11 +0900

    #10 refactor: enum 타입을 varchar형으로 저장

--- src/main/resources/mysql/data.sql
+++ src/main/resources/mysql/data.sql
@@ -1,11 +1,11 @@
 INSERT INTO host(id, email, phone, first_name, last_name, nick_name, encrypted_password, auth_status, membership_status)
-VALUES (1, 'test1@gmail.com', '8211112222', 'test1', 'test1', 'test1', 'test1@#524s2R', 0, 0);
+VALUES (1, 'test1@gmail.com', '8211112222', 'test1', 'test1', 'test1', 'test1@#524s2R', 'UNAUTHENTICATED', 'BASIC');
 
 INSERT INTO host(id, email, phone, first_name, last_name, nick_name, encrypted_password, auth_status, membership_status)
-VALUES (2, 'test2@gmail.com','8211112222', 'test2', 'test2', 'test2', 'test2@#524s2R', 0, 0);
+VALUES (2, 'test2@gmail.com','8211112222', 'test2', 'test2', 'test2', 'test2@#524s2R', 'UNAUTHENTICATED', 'BASIC');
 
 INSERT INTO guest(id, email, phone, first_name, last_name, nick_name, encrypted_password, birth, auth_status, count_monthly_stamp, count_coupon)
-VALUES (1, 'test1@gmail.com','8211112222', 'test1', 'test1', 'test1', 'test1@#524s2R', '2022-06-21', 0, 0, 0);
+VALUES (1, 'test1@gmail.com','8211112222', 'test1', 'test1', 'test1', 'test1@#524s2R', '2022-06-21', 'UNAUTHENTICATED', 0, 0);
 
 INSERT INTO guest(id, email, phone, first_name, last_name, nick_name, encrypted_password, birth, auth_status, count_monthly_stamp, count_coupon)
-VALUES (2, 'test2@gmail.com','8211112222', 'test2', 'test2', 'test2', 'test2@#524s2R', '2022-06-21', 0, 0, 0);
+VALUES (2, 'test2@gmail.com','8211112222', 'test2', 'test2', 'test2', 'test2@#524s2R', '2022-06-21', 'UNAUTHENTICATED', 0, 0);
