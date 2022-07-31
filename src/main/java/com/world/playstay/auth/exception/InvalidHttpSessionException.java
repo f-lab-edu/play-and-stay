7ee40e69f70fa10b0c8295d1ac174443fd3eac61
syworld <syworldy@gmail.com>
2022-07-31 20:17:24 +0900

    #9 refactor: static message 명시

--- src/main/java/com/world/playstay/auth/exception/InvalidHttpSessionException.java
+++ src/main/java/com/world/playstay/auth/exception/InvalidHttpSessionException.java
@@ -6,8 +6,13 @@ import org.springframework.http.HttpStatus;
 
 public class InvalidHttpSessionException extends GlobalHttpException {
 
+  private static final String MESSAGE = "Invalid Http Session";
+
+  public InvalidHttpSessionException() {
+    super(HttpStatus.NOT_FOUND, MESSAGE, LogLevel.ERROR);
+  }
+
   public InvalidHttpSessionException(String message) {
     super(HttpStatus.NOT_FOUND, message, LogLevel.ERROR);
   }
-
 }
