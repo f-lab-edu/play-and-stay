0454eacae16f6464569d7bfd3c1df16ece631989
syworld <syworldy@gmail.com>
2022-08-30 15:25:50 +0900

    #9 refactor: Add LoginResponse builder

--- src/main/java/com/world/playstay/auth/dto/request/LoginRequest.java
+++ src/main/java/com/world/playstay/auth/dto/request/LoginRequest.java
@@ -1,8 +1,6 @@
 package com.world.playstay.auth.dto.request;
 
-import com.world.playstay.auth.dto.response.LoginResponse;
-import com.world.playstay.user.enums.UserType;
-import java.time.LocalDateTime;
+import com.world.playstay.auth.enums.LoginMethod;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -20,15 +18,5 @@ public class LoginRequest {
   private final String password;
 
   @NotNull
-  private final UserType type;
-
-  public LoginResponse toLoginResponse(Long id, LocalDateTime date) {
-    return LoginResponse.builder()
-        .id(id)
-        .email(this.getEmail())
-        .type(this.getType())
-        .loginAt(date)
-        .build();
-  }
-
+  private final LoginMethod loginMethod;
 }
