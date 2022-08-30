a86edf3fa54c33d5cfba42ccd05bff3f8551566a
syworld <syworldy@gmail.com>
2022-08-30 15:30:42 +0900

    #9 refactor: InvalidHttpSessionException logging 처리 후 기본 에러 클래스 메시지 응답으로 변경

--- src/main/java/com/world/playstay/auth/service/SessionLoginService.java
+++ src/main/java/com/world/playstay/auth/service/SessionLoginService.java
@@ -1,8 +1,8 @@
 package com.world.playstay.auth.service;
 
 import com.world.playstay.auth.constant.SessionConstant;
-import com.world.playstay.auth.dto.request.LoginRequest;
 import com.world.playstay.auth.exception.InvalidHttpSessionException;
+import com.world.playstay.user.enums.UserType;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
@@ -12,38 +12,48 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class SessionLoginService implements LoginService {
 
-  private static final String SESSION_LOG_FORMAT = "[HttpSession] method: {} | type: {} | email: {} | message: {}";
+  private static final String SESSION_LOG_INFO_FORMAT = "[HttpSession] User Type = {} Email = {} {}";
+  private static final String SESSION_LOG_ERROR_FORMAT = "[HttpSession] {}";
 
-  public boolean setLoginStatus(HttpServletRequest httpServletRequest, LoginRequest loginRequest) {
+  @Override
+  public void setLoginStatus(HttpServletRequest httpServletRequest,
+      String email,
+      UserType userType) {
     HttpSession httpSession = httpServletRequest.getSession(true);
 
-    httpSession.setAttribute(SessionConstant.SESSION_ID, loginRequest.getEmail());
-    httpSession.setAttribute(SessionConstant.SESSION_USER_TYPE, loginRequest.getType());
+    httpSession.setAttribute(SessionConstant.SESSION_ID, email);
+    httpSession.setAttribute(SessionConstant.SESSION_USER_TYPE, userType);
 
-    log.info(SESSION_LOG_FORMAT, "set", loginRequest.getType(), loginRequest.getEmail(),
+    log.info(SESSION_LOG_INFO_FORMAT, userType,
+        email,
         "Login Successfully");
-    return true;
   }
 
-  public boolean validateLoginStatus(HttpServletRequest httpServletRequest) {
+  @Override
+  public void validateLoginStatus(HttpServletRequest httpServletRequest) {
     HttpSession httpSession = httpServletRequest.getSession(false);
     try {
-      return httpSession.getAttribute(SessionConstant.SESSION_ID) != null
-          && httpSession.getAttribute(SessionConstant.SESSION_USER_TYPE) != null;
+      if (httpSession.getAttribute(SessionConstant.SESSION_ID) == null
+          || httpSession.getAttribute(SessionConstant.SESSION_USER_TYPE) == null) {
+        throw new InvalidHttpSessionException("Session keys not found");
+      }
     } catch (Exception e) {
-      throw new InvalidHttpSessionException(e.getMessage());
+      log.error(SESSION_LOG_ERROR_FORMAT, e.getMessage());
+      throw new InvalidHttpSessionException();
     }
   }
 
+
+  @Override
   public void invalidateLoginStatus(HttpServletRequest httpServletRequest) {
     HttpSession httpSession = httpServletRequest.getSession(false);
     if (httpSession != null) {
       httpSession.invalidate();
-      log.info(SESSION_LOG_FORMAT, "invalidate",
+      log.info(SESSION_LOG_INFO_FORMAT,
           httpSession.getAttribute(SessionConstant.SESSION_USER_TYPE),
           httpSession.getAttribute(SessionConstant.SESSION_ID), "Logout Successfully");
     } else {
-      throw new InvalidHttpSessionException("Invalid session");
+      throw new InvalidHttpSessionException("Session not found");
     }
   }
 
